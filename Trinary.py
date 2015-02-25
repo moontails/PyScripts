f41beaddf9a5af238aa28ceed901f0f0eecf3516
Balachander Ramachandran <dragontransformer@gmail.com>
2015-02-25 11:42:08 -0600

    Update Trinary.py
    added comments

--- Trinary.py
+++ Trinary.py
@@ -1,11 +1,15 @@
 class Trinary_Tree:
+    
+    
     def __init__(self, val=None):
+        '''Function to intialise'''
         self.value = val
         self.left_child = None
         self.middle_child = None
         self.right_child = None
     
     def insert_Left(self, value):
+        '''Function to insert a left child'''
         #Base Case
         if self.left_child == None:
             self.left_child = Trinary_Tree(value)
@@ -18,6 +22,7 @@ class Trinary_Tree:
                 self.left_child.insert_Right(value)
         
     def insert_Middle(self, value):
+        '''Function to insert a middle child'''
         #Base Case
         if self.middle_child == None:
             self.middle_child = Trinary_Tree(value)
@@ -30,6 +35,7 @@ class Trinary_Tree:
                 self.middle_child.insert_Right(value)
                 
     def insert_Right(self, value):
+        '''Function to insert a right child'''
         # Base Case
         if self.right_child == None:
             self.right_child = Trinary_Tree(value)
@@ -42,6 +48,7 @@ class Trinary_Tree:
                 self.right_child.insert_Right(value)
     
     def display(self):
+        '''Function to dispay the tree by preorder traversal'''
         if self.value == None:
             return
         print str(self.value) + "\t"
@@ -61,9 +68,13 @@ if __name__ == "__main__":
         numbers.append(int(raw_input("Enter the number: ")))
         choice = raw_input("Do you wish to continue [Y/N]: ")
         
+    # Make the first number as root
     root = Trinary_Tree(numbers[0])
     
+    # Obtain the number of input numbers.
     n = len(numbers)
+    
+    # Process the remaining numbers
     for i in range(1,n):
         if numbers[i] < root.value:
             root.insert_Left(numbers[i])
@@ -72,4 +83,5 @@ if __name__ == "__main__":
         else:
             root.insert_Right(numbers[i])
     
+    # display the trinary tree      
     root.display()
