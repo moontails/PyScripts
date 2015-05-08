cedb821b90597b1f4b6d737db3ae52c7c6bc0762
moontails <dragontransformer@gmail.com>
2015-05-08 08:30:49 -0500

    minor change

--- get_lyrics.py
+++ get_lyrics.py
@@ -2,7 +2,7 @@
 
 #####################
 # argument 1 - Song Name, arguement 2 - Band Name
-# example uasge - 
+# example uasge -
 #         python get_lyrics.py "worth fighting for" "judas priest"
 #####################
 
@@ -17,35 +17,35 @@ band_name = ""
 def get_lyrics(song,band):
 
     # Constructing the url to fetch lyrics from
-    
+
     lyrics_url = "http://www.azlyrics.com/lyrics/" + band.replace(" ","") + "/" + song.replace(" ", "") + ".html"
-    
+
     #print lyrics_url + "\n"
 
     try:
-        
+
         # Open and read the page
-        
+
         page = urlopen(lyrics_url)
         html = page.read()
-        
+
         #Find the starting and ending indices for the lyrics
-        
-        startindex = html.find("<!-- start of lyrics -->") 
-        endindex   = html.find("<!-- end of lyrics -->") 
-        
+
+        startindex = html.find("<!-- start of lyrics -->")
+        endindex   = html.find("<!-- end of lyrics -->")
+
         # Slicing to get the lyrics
-        
+
         lyrics = html[startindex:endindex]
-        
+
         # Soupifying the page for better display
-        
+
         soup = BeautifulSoup(lyrics)
         print "\nHere is the lyrics for " + song.upper() + " by " + band.upper() + "\n"
         print soup.get_text()
-        
+
     except:
-        
+
         # Printing error message
         print "\nSorry " + song.upper() + " by " + band.upper() + " NOT FOUND\n"
 
